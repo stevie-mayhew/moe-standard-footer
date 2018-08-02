10f15422d8ad6b08673cf6bcb15afa7e040d4396
Will Rossiter <will@fullscreen.io>
2018-08-02 13:22:01 +1200

    Correct namespaces

--- src/Model/EducationSocialMediaLink.php
+++ src/Model/EducationSocialMediaLink.php
@@ -5,7 +5,8 @@ namespace Education\Core\Model;
 use SilverStripe\Forms\HeaderField;
 use SilverStripe\Forms\OptionsetField;
 use SilverStripe\Forms\DropdownField;
-use Education\StandardFooter\AbstractLinkObject;
+use Education\StandardFooter\Model\AbstractLinkObject;
+use Education\StandardFooter\Extension\EducationSiteConfigableObjectExtension;
 use Education\Core\Extension\SiteConfigableObjectExtension;
 
 class EducationSocialMediaLink extends AbstractLinkObject
