765ff0009fb4b64d5d8b6cdf062015f195ea751a
Will Rossiter <will@fullscreen.io>
2018-08-09 10:39:07 +1200

    Fix up unit tests and use new packagist server

--- tests/FooterSiteConfigTest.php
+++ tests/FooterSiteConfigTest.php
@@ -5,9 +5,12 @@ namespace Education\StandardFooter\Tests;
 use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Forms\GridField\GridField;
 
 class FooterSiteConfigTest extends FunctionalTest
 {
+    protected static $fixture_file = 'fixtures.yml';
+
     public function testCMSFields()
     {
         $config = SiteConfig::current_site_config();
